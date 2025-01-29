e4ae0ecea91de47f1dff48c4c30f51ac897da688
jambu <kadamjd99@gmail.com>
2025-01-29 21:32:39 +0530

    init project

---
+++ src/main/java/com/qpro/groceryapi/GroceryapiApplication.java
@@ -0,0 +1,13 @@
+package com.qpro.groceryapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroceryapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroceryapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qpro/groceryapi/GroceryapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qpro.groceryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroceryapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
